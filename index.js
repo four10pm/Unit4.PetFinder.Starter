e782156e21c4129339ea3186c8e7391af2de2dff
Paulina <paulinasfielek@gmail.com>
2024-01-04 12:38:33 -0800

    pet finder

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname + '/public/index.html')
 });
 
 // hello world route
@@ -21,31 +21,47 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets)
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const owner = req.query.owner
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.send(
+      `Pet ID: ${pet.id} </br> 
+       Pet name: ${pet.name} </br>
+       Pet breed: ${pet.breed} </br>
+       Pet age: ${pet.age} </br>
+       Pet owner: ${pet.owner} </br>
+       Owner telephone number: ${pet.telephone} </br>
+       Upcoming appointments: ${pet.appointments[0].date} at ${pet.appointments[0].time} for ${pet.appointments[0].reason}`
+    )
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const name = req.params.name
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    res.send(
+      `Pet ID: ${pet.id} </br>
+       Pet name: ${pet.name} </br>
+       Pet breed: ${pet.breed} </br>
+       Pet age: ${pet.age} </br>
+       Pet owner: ${pet.owner} </br>
+       Owner telephone number: ${pet.telephone} </br>
+       Upcoming appointments: ${pet.appointments[0].date} at ${pet.appointments[0].time} for ${pet.appointments[0].reason}`
+    )
 });
 
 app.listen(PORT, () => {
---
+++ public/script.js
@@ -0,0 +1,10 @@
+const petinfo = document.querySelector("petinfo")
+
+const petform = document.querySelector("petform")
+petform.addEventListener("submit", onPetSubmit )
+const onPetSubmit = (value) => {
+
+}
+
+
+const ownerform = document.querySelector("ownerform")
